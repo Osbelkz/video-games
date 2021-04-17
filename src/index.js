aceb872100d45938664c40e893f0cb373b72490a
Askar <osbelkz@gmail.com>
2021-04-17 15:41:17 +0600

    hash router

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './ui/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./bll/store";
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-          <BrowserRouter>
+          <HashRouter>
               <App />
-          </BrowserRouter>
+          </HashRouter>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
